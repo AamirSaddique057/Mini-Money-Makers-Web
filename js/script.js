c81eb5d7cb76b3989def764e0d796ea6f65c444e
Aamir Saddique <aamir1saddique@gmail.com>
2026-04-09 16:49:45 +0500

    All Pages created 'about page, blog page, footer page, header page, and orther css js index file changes are done'

--- js/script.js
+++ js/script.js
@@ -1,10 +1,28 @@
 // Slider Buttons
-const wrapper = document.querySelector('.blog-wrapper');
+// const wrapper = document.querySelector('.blog-wrapper');
 
-document.getElementById('next').onclick = () => {
-    wrapper.scrollLeft += 320;
-};
+// document.getElementById('next').onclick = () => {
+//     wrapper.scrollLeft += 320;
+// };
 
-document.getElementById('prev').onclick = () => {
-    wrapper.scrollLeft -= 320;
-};
+// document.getElementById('prev').onclick = () => {
+//     wrapper.scrollLeft -= 320;
+// };
+
+// header and footer snippet call in every page
+// fetch('header.html')
+//     .then(response => response.text())
+//     .then(data => {
+//         document.getElementById('header').innerHTML = data;
+//     });
+
+    function loadComponent(id, file) {
+    fetch(file)
+        .then(res => res.text())
+        .then(data => {
+        document.getElementById(id).innerHTML = data;
+        });
+    }
+
+    loadComponent("header", "header.html");
+    loadComponent("footer", "footer.html");
